fa91a27497e675f4c9dc66227f7a8a3fc89a66c4
NitinVibe <creativenitin11@gmail.com>
2025-04-06 22:36:46 +0530

    updated

---
+++ expence.py
@@ -0,0 +1,18 @@
+class Expense:
+    def __init__(self, name, category, ammount):
+        self.name = name
+        self.category = category
+        self.ammount = ammount
+        input("enter expence name")
+    
+#     def get_price(self, weight_kg):
+#         return self.price_per_kg * weight_kg
+        
+# banana =Expense("🍌", calorie=105, price_per_kg=40)
+# tomato =Expense("🍅",calorie=30,price_per_kg=30)
+# apple=Expense("🍎",calorie=50,price_per_kg=100)
+# print(banana.name,banana.calorie)
+# print(tomato.name,tomato.calorie)
+# print(apple.name,apple.calorie)
+
+# print(banana.get_price(2))
---
+++ expence_tracker.py
@@ -0,0 +1,44 @@
+def main ():
+    print(f"running expence tracker")
+    
+# get user input for expence
+    get_user_expence()
+
+# write their epence to a file
+    save_expence_to_file()
+# read files and summarise expences.
+    summarize_expences()
+
+
+def get_user_expence():
+    print(f"Getting User Expence")
+    expense_name = input("Enter Expence name:")
+    expense_ammount = float(input("Enter Expence Ammount:"))
+    print(f"you'r entered {expense_name}, {expense_ammount}")
+
+    expense_category = [
+        "🍟food",
+        "🏠home", 
+        "🏢work", 
+        "🎉fun", 
+        "✨misc"
+    ]
+    while True:
+        print("Select Category:")
+        for i, category_name in enumerate(expense_category):
+            print(f"{i+1}. {category_name}")
+        value_range = f"[1-{len(expense_category)}]"
+        selected_index = int(input(f"enter a category number{value_range}:"))
+
+        break
+
+def save_expence_to_file():
+    print(f"Saving User Expences")
+    
+
+def summarize_expences():
+    print(f"Summarise User Expences")
+    
+
+if __name__ =="__main__":
+    main()
