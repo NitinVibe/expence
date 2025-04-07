10cc59c29205a4be0de77e4885bbe5014adc39f7
NitinVibe <creativenitin11@gmail.com>
2025-04-07 12:50:49 +0530

    updated againnnn

--- expence.py
+++ expence.py
@@ -4,7 +4,7 @@ class Expense:
         self.category = category
         self.ammount = ammount
         input("enter expence name")
-print("check me")
+print("check me again")
     
 #     def get_price(self, weight_kg):
 #         return self.price_per_kg * weight_kg
